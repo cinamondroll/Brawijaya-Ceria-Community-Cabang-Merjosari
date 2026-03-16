7412362896682c157ac8e5d7b02d33e3b804d7b7
Alvianto Hery Sarborn <alfiantoherysarborn@gmail.com>
2026-03-16 20:00:28 +0700

    POST SELESAI. Sekalian sesuaikan dengan punyanya naya

--- app/Http/Controllers/ToDoListController.php
+++ app/Http/Controllers/ToDoListController.php
@@ -6,22 +6,38 @@ use Illuminate\Http\Request;
 
 class ToDoListController extends Controller
 {
+    // HERY: Pake dengan format ini aja nanti
+    // POST /api/todo-list
+    public function store(Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'id' => 'required|integer|min:1',
+                'title' => 'required|string|max:100',
+                'description' => 'required|string|max:255',
+                'deadline' => 'required|date_format:Y-m-d',
+                'priority' => 'required|in:low,medium,high',
+                'status' => 'required|in:pending,in_progress,completed',
+            ]);
+        } catch (\Illuminate\Validation\ValidationException $th) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $th->validator->errors(),
+            ], 422);
+        }
 
+        return response()->json([
+            'message' => "Todo list {$validated['title']} berhasil dihapus",
+            'data' => $validated,
+        ], 201);
+
+    }
 
     //Naya: ini masih dummy data, nanti sambung sambungin lah sama data create (kalo bisa)
     public function destroy($id)
-{
-    $todo = [
-        "id" => "1",
-        "title" => "Mengerjakan laporan praktikum",
-        "description" => "Menyelesaikan laporan praktikum TIS",
-        "deadline" => "2026-03-20",
-        "priority" => "high",
-        "status" => "pending"
-    ];
-
-    return response()->json([
-        "message" => "List {$todo['title']} deleted successfully"
-    ]);
-}
+    {
+        return response()->json([
+            "message" => "Todo-list {$id} berhasil dihapus"
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ToDoListController;
 
-//DETELE
+//NAYA: DETELE
 Route::delete('/todo-lists/{id}', [ToDoListController::class, 'destroy']);
+//HERY: POST
+Route::post('/todo-list', [ToDoListController::class, 'store']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
