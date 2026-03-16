0ff18f2a11578ad74931e83d87819f1b70ff4cc0
misuaa <nurulinayahh19@student.ub.ac.id>
2026-03-16 13:55:37 +0700

    Add: Routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ToDoListController;
+
+//DETELE
+Route::delete('/todo-lists/{id}', [ToDoListController::class, 'destroy']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
