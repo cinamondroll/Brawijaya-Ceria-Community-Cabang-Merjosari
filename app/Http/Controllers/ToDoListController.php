ebc925263fbaa22487cfa7fd91eaea07b523cff5
NaNoLimit7 <malamharioo123@gmail.com>
2026-03-16 21:18:56 +0700

    GET selesai

--- app/Http/Controllers/ToDoListController.php
+++ app/Http/Controllers/ToDoListController.php
@@ -27,10 +27,9 @@ class ToDoListController extends Controller
         }
 
         return response()->json([
-            'message' => "Todo list {$validated['title']} berhasil dihapus",
+            'message' => "Todo list {$validated['title']} berhasil ditambahkan",
             'data' => $validated,
         ], 201);
-
     }
 
     //Naya: ini masih dummy data, nanti sambung sambungin lah sama data create (kalo bisa)
@@ -39,5 +38,63 @@ class ToDoListController extends Controller
         return response()->json([
             "message" => "Todo-list {$id} berhasil dihapus"
         ]);
+    }
+
+    public function index()
+    {
+        $todos = $this->getDummyData();
+
+        return response()->json([
+            'message' => 'Berhasil mengambil semua data Todo list',
+            'data' => $todos
+        ]);
+
+    }
+
+    public function show($id)
+    {
+
+        $todos = $this->getDummyData();
+
+        $simpanDataTodo = null;
+
+        foreach ($todos as $todo) {
+            if($todo['id'] == $id){
+                $simpanDataTodo = $todo;
+                break;
+            }
+        }
+
+        if ($simpanDataTodo != null) {
+            return response()->json([
+                'message' => 'Todo list berhasil ditemukan',
+                'data' => $simpanDataTodo
+            ],200);
+        }
+
+        return response()->json([
+            "message" => "Todo list dengan id {$id} tidak ditemukan"
+        ], 404);
+    }
+
+    private function getDummyData(){
+        return [
+            [
+                "id" => 1,
+                "title" => "Tugas membuat ayam krispi",
+                "description" => "Membuat ayam krispi yang sangat mirip atau identik dengan ayam krispi kfc",
+                "deadline" => "2026-3-17",
+                "priority" => "high",
+                "status" => "in_progress"
+            ],
+            [
+                "id" => 2,
+                "title" => "Tugas membuat Nasi goreng kediri",
+                "description" => "membuat nasi goreng seenak penjual nasi goreng di depan bandung collection",
+                "deadline" => "2026-3-18",
+                "priority" => "medium",
+                "status" => "pending"
+            ]
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,9 @@ use App\Http\Controllers\ToDoListController;
 Route::delete('/todo-lists/{id}', [ToDoListController::class, 'destroy']);
 //HERY: POST
 Route::post('/todo-list', [ToDoListController::class, 'store']);
+//RAKHMAN: GET
+Route::get('/todo-list', [ToDoListController::class, 'index']);
+Route::get('/todo-list/{id}', [ToDoListController::class, 'show']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
